2762cb7dc34e2c830db2980317fb39d4fceeb846
MarkParayno1004 <paraynomarkphilip@gmail.com>
2024-07-01 18:47:38 +0800

    Today Lessons

---
+++ exercises/for_loop.py
@@ -0,0 +1,7 @@
+prices = [10, 20 ,30]
+
+
+for price in prices:
+    price += price
+
+print(f"total price is ${price}")
---
+++ lessons/for_loops.py
@@ -0,0 +1,11 @@
+for item in 'Python':
+    print(item)
+
+for num in [1,2,3,4,5]:
+    print(num)
+    
+for names in ['Mark', 'Philip', 'John']:
+    print(names)
+
+for from_to_number in range(5,10,2):
+    print(from_to_number)
